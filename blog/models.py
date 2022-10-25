c6679a6b642db004cb1537134fc6faa5cf13f94a
dad-siberian <dad.siberian@gmail.com>
2022-10-25 07:51:04 +0300

    move code to reusable QuerySet method

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.db.models import Count
+
+
+class PostQuerySet(models.QuerySet):
+
+    def year(self, year):
+        posts_at_year = self.filter(published_at__year=year) \
+                            .order_by('published_at')
+        return posts_at_year
+
+
+class TagQuerySet(models.QuerySet):
+
+    def popular(self):
+        most_popular_tags = self.annotate(tags_count=Count('posts')) \
+                                        .order_by('-tags_count')
+        return most_popular_tags
+
 
 
 class Post(models.Model):
@@ -24,6 +42,7 @@ class Post(models.Model):
         'Tag',
         related_name='posts',
         verbose_name='Теги')
+    objects = PostQuerySet.as_manager()
 
     def __str__(self):
         return self.title
@@ -39,6 +58,7 @@ class Post(models.Model):
 
 class Tag(models.Model):
     title = models.CharField('Тег', max_length=20, unique=True)
+    objects = TagQuerySet.as_manager()
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -63,8 +63,7 @@ def index(request):
         Prefetch('author')).annotate(
             comments_count=Count('comments')).order_by('-published_at')
 
-    most_popular_tags = Tag.objects.annotate(
-        num_tags=Count('posts')).order_by('-num_tags')[:5]
+    most_popular_tags = Tag.objects.popular()[:5]
 
     context = {
         'most_popular_posts': [
@@ -105,9 +104,7 @@ def post_detail(request, slug):
         'tags': [serialize_tag(tag) for tag in related_tags],
     }
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = Tag.objects.popular()[:5]
 
     most_popular_posts = []  # TODO. Как это посчитать?
 
@@ -124,9 +121,7 @@ def post_detail(request, slug):
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = Tag.objects.popular()[:5]
 
     most_popular_posts = []  # TODO. Как это посчитать?
 
